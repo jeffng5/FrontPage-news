626fe6636fa550b916dd957f99e0f67908c51f42
jeffng5 <jeffreykng@hotmail.com>
2024-02-26 06:21:54 -0500

    corrected test db

--- backend/news_test.sql
+++ backend/news_test.sql
@@ -30,3 +30,11 @@ CREATE TABLE preferences (
     username TEXT NOT NULL,
     preferences TEXT NOT NULL
 )
+
+CREATE TABLE comments (
+    id BIGSERIAL PRIMARY KEY,
+    comment TEXT NOT NULL,
+    username TEXT NOT NULL,
+    forum_art_id INTEGER REFERENCES forum(id) ON DELETE CASCADE,
+    likes INTEGER default 0
+)
