1555dd2e8e6cc3975f62e9bff350817694e34457
jeffng5 <jeffreykng@hotmail.com>
2024-03-02 15:13:22 -0500

    changed port

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,6 @@
 const app = require("./app");
 const { PORT } = require("./config");
 
-app.listen(3001, function () {
-  console.log(`Started on http://localhost:${PORT}`);
+app.listen(10000, function () {
+  console.log(`Started on http://0.0.0.0:10000`);
 });
