9c76ef0ac7b83f033f5b9d595b11ee5dc42a149e
jeffng5 <jeffreykng@hotmail.com>
2024-03-17 10:00:59 -0400

    fixed links

--- news-aggregator/src/Cards/ArticleCard.js
+++ news-aggregator/src/Cards/ArticleCard.js
@@ -9,22 +9,22 @@ import ButtonForum from "../Buttons/ButtonForum"
 // Card template to hold the articles returned
 
 //receiving prop data from parent
-const ArticleCard = ({title, excerpt, link, author, media}) => {
+const ArticleCard = ({title, excerpt, url, author, media}) => {
     let username = localStorage.getItem('username')
 
-  
+    console.log({url})
     return (
     <>
   
     <div className='article'>
     <p><img className = 'photograph' src={media} alt = 'news'/></p>
-    <h2 className='headline' id='headline'><span><a href={link} target="_blank">{title}<h5 className='desc'>{excerpt}</h5></a></span></h2>
+    <h2 className='headline' id='headline'><span><a href={url} target="_blank">{title}<h5 className='desc'>{excerpt}</h5></a></span></h2>
     </div>
     <div className='button-6'>
-    <Archive username={username} link={link} title={title} excerpt={excerpt} author={author}/>
+    <Archive username={username} link={url} title={title} excerpt={excerpt} author={author}/>
     </div>
     <div className='button-6'>
-    <ButtonForum username={username} title={title} excerpt={excerpt} link={link} author={author} media={media}/></div>
+    <ButtonForum username={username} title={title} excerpt={excerpt} link={url} author={author} media={media}/></div>
     <div className='author-2'>by: {author}</div>
     <div className='line'>
     <ColoredLineThin color = "gray" />  
--- news-aggregator/src/MainComponents/Welcome.js
+++ news-aggregator/src/MainComponents/Welcome.js
@@ -7,23 +7,23 @@ const Welcome = () => {
 
     return (
         <>
-        <body className='mobile-users'>
-        <h1 className='welcome'>Welcome to</h1> <p className= 'news-title'><b className = 'title'>YourFrontPageNews.com</b></p>
+            <body className='mobile-users'>
+                <h1 className='welcome'>Welcome to</h1> <p className='news-title'><b className='title'>YourFrontPageNews.com</b></p>
 
-        <h2><div><b></b></div>
-        <div className="intro">
-            your curated articles of your news preferences</div> <div className='intro'>from the <b>top news publications</b></div></h2>
+                <h2><div><b></b></div>
+                    <div className="intro">
+                        your curated articles of your news preferences</div> <div className='intro'>from the <b>top news publications</b></div></h2>
 
-      
-        <span id='login'>
-        <Link to = "/login"><p className='top'>Login</p></Link>
-        </span>
-        <span id='login'>
-        <Link to = "/signup"><p className='top-1'>Register</p></Link>
-        </span>
-         
-    </body>
-    </>
+
+                <span id='login'>
+                    <Link to="/login"><p className='top'>Login</p></Link>
+                </span>
+                <span id='login'>
+                    <Link to="/signup"><p className='top-1'>Register</p></Link>
+                </span>
+
+            </body>
+        </>
     )
 }
 
