20e6d8ec719dfcd1767d565a97e1e217824e1d38
jeffng5 <jeffreykng@hotmail.com>
2024-06-14 18:10:01 -0400

    added db connection string

--- backend/db.js
+++ backend/db.js
@@ -11,7 +11,7 @@ const DATABASE_PORT = process.env.DATABASE_PORT;
 
 if (process.env.NODE_ENV === "production") {
   db = new Client({
-    connectionString: `postgresql://${USER}:${PASSWORD}@127.0.0.1:${DATABASE_PORT}/news`,
+    connectionString: `postgres://postgres_news_db_user:14vFZYHBnS0FT8ph1HHCtS6eAIlUg24q@dpg-cnjk5o6v3ddc738dcmcg-a.oregon-postgres.render.com/postgres_news_db`,
   
     ssl: {
       rejectUnauthorized: false
