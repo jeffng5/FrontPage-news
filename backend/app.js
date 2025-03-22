f48799ccde13d88a57aab6335f66e1c375b5244e
jeffng5 <jeffrey.ng51213@outlook.com>
2025-03-22 15:47:11 -0400

    trying debug cors

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ const authorization = (req, res, next ) => {
 
 const handleCors = (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-     res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'OPTIONS')
+  res.header('Access-Control-Allow-Methods', '*');
   res.header("Access-Control-Allow-Headers", 'Content-Type', 'Authorization');
   next();
  };
