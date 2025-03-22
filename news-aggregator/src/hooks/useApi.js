8d55722bd27b8ee71c128a31049e02f6c7e5d83b
jeffng5 <jeffrey.ng51213@outlook.com>
2025-03-22 14:31:34 -0400

    changed api but reverted css changes

--- news-aggregator/src/hooks/useApi.js
+++ news-aggregator/src/hooks/useApi.js
@@ -3,12 +3,13 @@ import axios from 'axios'
 import ArticleCard from "../Cards/ArticleCard"
 import ColoredLine from "../SmallComponents/ColoredLine"
 
-const useApi = () => {
 
+const useApi = () => {
     const apiKey = process.env.REACT_APP_APIKEY
+  
     let pref = localStorage.getItem('preferences')
     let subj = pref ? pref.split(',') : "";
-
+    console.log('PREFS', subj)
     const [article, setArticle] = useState([])
     const [article1, setArticle1] = useState([])
     const [search, setSearch] = useState([])
@@ -28,101 +29,117 @@ const useApi = () => {
         }
 // API call for country's articles
         for (const [key, value] of Object.entries(mySearchParams)) {
-
+            
             let options = {
-                method: 'GET',
-                url: `https://api.newscatcherapi.com/v2/latest_headlines?countries=${value}&lang=en&page=1`,
-                headers: { 'x-api-key': apiKey }
+            method : 'GET',
+            url : `https://newsapi.org/v2/top-headlines?country=${value}&lang=en&page=1`,
+            headers : { 'x-api-key': apiKey, 'Content-Type' : 'application/json' }
             }
-            if (subj[0] === key || subj[1] === key || subj[2] === key || subj[3] === key
-                || subj[4] === key) {
+            
+            if (subj[0] == key || subj[1] == key || subj[2] == key || subj[3] == key
+                || subj[4] == key) {
                 axios.request(options).then(function (response) {
+                    console.log(response);
                     console.log(response.data.articles);
                     setArticle(response.data.articles)
                 }).catch(function (error) { console.error(error) })
             }
 
         }
-    }
+   }
 
 // API Call for articles according to topic
     function findArticlesByTopic() {
+        
         const myTopicParams = {
+
             'Business': 'business',
             'Entertainment': 'entertainment',
             'General': 'news',
             'Food': 'food',
             'Science': 'science',
-            'Sports': 'sport',
-            'Technology': 'tech'
+            'Sports': 'sports',
+            'Technology': 'technology',
+           
         }
         for (const [key, value] of Object.entries(myTopicParams)) {
-
+            
             let options1 = {
-                method: 'GET',
-                url: `https://api.newscatcherapi.com/v2/latest_headlines?topic=${value}&lang=en&page=1`,
-                headers: { 'x-api-key': apiKey }
+            method: 'GET',
+            url : `https://newsapi.org/v2/everything?q=${value}`,
+            headers : { 'x-api-key': apiKey, 'Content-Type' : 'application/json' }
             }
-
-            if (subj[0] === key || subj[1] === key || subj[2] === key || subj[3] === key
-                || subj[4] === key) {
-                axios.request(options1).then(function (response) {
-                    console.log(response.data.articles);
-                    setArticle1(response.data.articles)
-                }).catch(function (error) { console.error(error) })
+            
+            if (subj[0] == key || subj[1] == key || subj[2] == key || subj[3] == key
+                || subj[4] == key) 
+            
+                {
+                axios.request(options1).then(function (response){
+                console.log(response.status)
+                console.log(response.data.articles);
+                setArticle1(response.data.articles);
+                }).catch((error) => { console.error(error) }
+                )
             }
         }
-    }
+        }
+    
 
 // API Call to articles according to a search term    
     function findArticlesBySearchTerm() {
         let term = localStorage.getItem('freePreferences')
-        let options11 = {
-            method: 'GET',
-            url: 'https://api.newscatcherapi.com/v2/search',
-            params: { q: term, countries: 'US', lang: 'en', page: '1' },
-            headers: { 'x-api-key': apiKey }
-        }
-
-        try {
-            axios.request(options11).then(function (response) {
+        console.log(term)
+        let search = {
+            method : 'GET', 
+            url :`https://newsapi.org/v2/everything?q=${term}`,
+            params : { lang:'en' , page: '1'},
+            headers : { 'x-api-key': apiKey, 'Content-Type' : 'application/json' } 
+        }  
+            try {
+            axios.request(search).then(function (response){
+                console.log(response.status);
                 console.log(response.data.articles);
-                setSearch(response.data.articles)
-                localStorage.removeItem('freePreferences')
+                setSearch(response.data.articles);
+                // localStorage.removeItem('freePreferences')
             }).catch(function (error) { console.error(error) })
-        } catch (e) {
+        } catch(e) {
             console.log(e)
         }
     }
-
+        console.log({article})
+        console.log({article1})
+        console.log({search})
     return (
 
+
+
         <>
+       
             {article.map(c => (
                 <ArticleCard
                     title={c.title}
-                    key={c.key}
-                    url={c.link}
-                    excerpt={c.excerpt}
-                    media={c.media}
+                    // key={c.key}
+                    url={c.url}
+                    excerpt={c.description}
+                    media={c.urlToImage}
                     author={c.author} />
             ))}
             {article1.map(c => (
                 <ArticleCard
                     title={c.title}
-                    key={c.key}
-                    url={c.link}
-                    excerpt={c.excerpt}
-                    media={c.media}
+                    // key={c.key}
+                    url={c.url}
+                    excerpt={c.description}
+                    media={c.urlToImage}
                     author={c.author} />
             ))}
             {search.map(c => (
                 <ArticleCard
                     title={c.title}
-                    key={c.key}
-                    url={c.link}
-                    excerpt={c.excerpt}
-                    media={c.media}
+                    // key={c.key}
+                    url={c.url}
+                    excerpt={c.description}
+                    media={c.urlToImage}
                     author={c.author} />
             ))}
 
