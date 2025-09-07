ac54b30640ff63279b1e463eac1e912e7d41b4c3
jeffng5 <jeffrey.ng51213@outlook.com>
2025-09-07 15:23:23 -0400

    finalized article display object working

--- news-aggregator/src/hooks/useApi.js
+++ news-aggregator/src/hooks/useApi.js
@@ -8,7 +8,6 @@ const useApi = () => {
     const apiKey = process.env.REACT_APP_APIKEY
     let pref = localStorage.getItem('preferences')
     let term = localStorage.getItem('freePreferences')
-    console.log('debugging', term)
     let subj = pref ? pref.split(',') : "";
     console.log('PREFS', subj)
     const [article, setArticle] = useState([])
@@ -16,12 +15,12 @@ const useApi = () => {
     const [search, setSearch] = useState('')
     const [error, setError] = useState('')
 
-    useEffect(() => {
+    useEffect(() => { 
         findArticlesByCountry();
         findArticlesByTopic();
-        findArticlesBySearchTerm();
-        setSearch([]);
-    }, [])
+        findArticlesBySearchTerm(term);
+        // setSearch('');
+     }, [])
     async function findArticlesByCountry() {
         const mySearchParams = {
             'Australia': 'au',
@@ -29,97 +28,92 @@ const useApi = () => {
             'U.K.': 'gb',
             'US': 'us',
         }
-// API call for country's articles
+        // API call for country's articles
         for (const [key, value] of Object.entries(mySearchParams)) {
-            console.log('key', key)
+
             let options = {
-            method : 'GET',
-            url : `https://api.thenewsapi.com/v1/news/top?api_token=${apiKey}&locale=${value}&limit=50&language=en`,
-            headers : { 'Content-Type' : 'application/json' },
-            // params : {
-            //     'limit' : 50,
-            //     'language': 'en'  
-            // }
+                method: 'GET',
+                url: `https://api.thenewsapi.com/v1/news/top?api_token=${apiKey}&locale=${value}&limit=50&language=en`,
+                headers: { 'Content-Type': 'application/json' },
+                // params : {
+                //     'limit' : 50,
+                //     'language': 'en'  
+                // }
             }
-            
+
             if (subj[0] === key || subj[1] === key || subj[2] === key || subj[3] === key
                 || subj[4] === key) {
                 let res = await axios.request(options)
-                console.log(res)
-                    setArticle(res.data.data)
-                }
- }
-            };
+                console.log(res.data.data)
+                const fetchedData = res.data.data   
+                setArticle((prev) => [...prev, ...fetchedData])
+                // }
+            }
+        }
+    };
 
-        
-   
 
-// API Call for articles according to topic
+
+
+    // API Call for articles according to topic
     async function findArticlesByTopic() {
-        
-        const myTopicParams = {
 
+        const myTopicParams = {
             'Business': 'business',
             'Entertainment': 'entertainment',
             'General': 'news',
             'Food': 'food',
             'Science': 'science',
             'Sports': 'sports',
-            'Technology': 'technology',
-           
+            'Technology': 'tech',
         }
 
-        for (const [k, v] of Object.entries(myTopicParams)){
-            
+        for (const [k, v] of Object.entries(myTopicParams)) {
+
             let options1 = {
-                method : 'GET',
-                url : `https://api.thenewsapi.com/v1/news/top?api_token=${apiKey}&locale=us&categories=${v}&limit=50&language=en`,
-                headers : { 'Content-Type' : 'application/json' },
+                method: 'GET',
+                url: `https://api.thenewsapi.com/v1/news/top?api_token=${apiKey}&categories=${v}&limit=50&language=en`,
+                headers: { 'Content-Type': 'application/json' },
                 //params : {
-                    // 'categories' : `${value}`,
+                // 'categories' : `${value}`,
                 //     'limit' : 50,
                 //     'api_token': apiKey
-               // }
-                }
-            
-            if (subj[0] === k || subj[1] === k || subj[2] === k || subj[3] ===k || subj[4] === k) {
-    
+                // }
+            }
+
+            if (subj[0] === k || subj[1] === k || subj[2] === k || subj[3] === k || subj[4] === k) {
                 let res = await axios.request(options1)
                 console.log(res.data.data)
-                
-                setArticle1(res.data.data);
-                
-
+                let topicData = res.data.data
+             
+                // if (res.length > 0) {
+                    setArticle1((prev) => [...prev, ...topicData]);
+                // }
             }
-        }
+        };
     };
-        
-    
 
-// API Call to articles according to a search term    
-    async function findArticlesBySearchTerm() {
-       
-        console.log('TERM', term)
+
+    // API Call to articles according to a search term    
+    async function findArticlesBySearchTerm(term) {
+
         let fetchSearch = {
-            method : 'GET', 
-            url :`https://api.thenewsapi.com/v1/news/top?api_token=${apiKey}&search=${term}&limit=50&language=en`,
-            // params : { 
-            //     'language' : 'en',
-            //     'limit' : 50 
-            // },
-            headers : { 'Content-Type' : 'application/json' } 
-        }  
-        if (term != null) {
-            try {
-      
-            let res = await axios.request(fetchSearch)
-                console.log(res)
-                setSearch(res.data.data);
-                localStorage.removeItem('freePreferences')
-            } catch(e) {
-        console.log(e)
-    }
-    };
+            method: 'GET',
+            url: `https://api.thenewsapi.com/v1/news/top?api_token=${apiKey}&search=${term}&limit=50&language=en`,
+            headers: { 'Content-Type': 'application/json' }
+        }
+        console.log('do you see me?')
+        let res = await axios.request(fetchSearch)
+        console.log('search API call', res)
+        if (res && fetchSearch) {
+            setSearch(res.data.data);
+            // localStorage.removeItem('freePreferences')
+        }
+
+        else {
+            console.log('You are down here')
+
+        }
     };
 
     // const arrayOfArticles = Object.entries(article)
@@ -128,22 +122,22 @@ const useApi = () => {
 
     console.log('SEARCH', search)
     console.log('ARTICLE1', article1)
-    console.log('ARTICLE',article)
+    console.log('ARTICLE', article)
 
-    let objectConcat = [...article, ...search, ...article1]
-    console.log({error})
+    let objectConcat = [...article, ...article1, ...search]
+
 
 
     return (
         <>
-       <h1>{error}</h1>
-                    
-                <ArticleCard items = {objectConcat} />
-    
+            <h1>{error}</h1>
+
+            <ArticleCard items={objectConcat} />
+
         </>
 
     )
-            };
+};
 
 
 export default useApi
